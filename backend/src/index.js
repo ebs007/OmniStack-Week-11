4495238146a13729d67bfb9154d97cad093e6068
Erick Silva <erick.silva@celsolisboa.edu.br>
2020-03-26 22:08:49 -0300

    aula 1 e aula 2

---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const routes = require('./routes')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+/*
+GET: Buscar/listar uma informação do back-end
+POST: Criar uma informação no back-end
+PUT: Alterar uma informação no back-end
+DELETE: Deletar uma informação no back-end
+
+Tipo de parâmetros:
+
+Query params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+Route Params: Parâmetros utilizados para identificar recursos
+Request Body: Corpo da requisição, utilizando para criar ou alterar recursos
+
+Driver: SELECT * FROM users
+Query Builder: table('users).select('*').where()
+
+NPM - Instalar um pacote
+NPX - Executar um pacote
+*/
+
+app.listen(3333)
